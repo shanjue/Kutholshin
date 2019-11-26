88f7c7f2b59aa4df3b0eac5f0368e5b060a9216d
sai myo min aung <saimyominaung2110@gmail.com>
2019-11-26 09:41:54 +0630

    websocket send message with channel_one

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,15 +9,35 @@
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
                     You are logged in!
+                </div>
+                <div class="card-body">
+                    
                 </div>
             </div>
         </div>
     </div>
 </div>
+@endsection
+
+@section('js')
+<script>
+    var conn = new WebSocket('ws://localhost:2000');
+
+    var message = document.getElementById('message');
+
+    conn.onopen = function(e) {
+        console.log("Connection established!");
+        conn.send(JSON.stringify({ command: "subscribe", channel: 'channel_one' }));
+    };
+
+    conn.onmessage = function(e) {
+        console.log(e.data);
+    };
+</script>
 @endsection
